2aaaea8e3ddde1802bfdf2cbfa43978e0b107d53
tuyizerejean <tuyibobu@gmail.com>
2022-02-01 22:06:36 +0200

    Working on validations

--- src/routes/api/commentRoutes.js
+++ src/routes/api/commentRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { CommentControllers } from "../../controllers/commentsController.js";
+import { commentValidation } from "../../validations/commentValidation/comment.validation.js";
 
 const route = express.Router();
 const commentsController = new CommentControllers();
-route.post("/:articleid", commentsController.addComment);
+route.post("/:articleid", commentValidation, commentsController.addComment);
 route.get("/:articleid", commentsController.getComments);
 
 export default route;
