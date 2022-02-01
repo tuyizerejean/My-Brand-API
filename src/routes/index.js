f1ee3240de2f12171dd1d1cc759bc672db605b4f
tuyizerejean <tuyibobu@gmail.com>
2022-02-01 16:50:22 +0200

    working  on selecting  Comment

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
-import express from 'express'
-import welcomeRoutes from "./api/welcomeRoutes.js"
-import articleRoutes from "./api/articleRoutes.js"
-import queriesRoutes from "./api/queriesRoutes.js"
-import userRoutes from "./api/userRoutes.js"
+import express from "express";
+import welcomeRoutes from "./api/welcomeRoutes.js";
+import articleRoutes from "./api/articleRoutes.js";
+import commentRoutes from "./api/commentRoutes.js";
+import queriesRoutes from "./api/queriesRoutes.js";
 
-const routes = express.Router()
+const routes = express.Router();
 
-routes.use('/', welcomeRoutes)
-routes.use('/aritcles', articleRoutes)
-routes.use('/queries', queriesRoutes)
-routes.use('/user', userRoutes)
+routes.use("/", welcomeRoutes);
+routes.use("/aritcles", articleRoutes);
+routes.use("/comments", commentRoutes);
+routes.use("/queries", queriesRoutes);
 
-export default routes
+export default routes;
