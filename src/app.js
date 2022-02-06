c6d5053c069813f3c23790f69f35d4e08c86f62d
tuyizerejean <tuyibobu@gmail.com>
2022-02-06 03:14:38 +0200

    MybrandSwagger

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import routes from "./routes/index.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 import "dotenv/config";
 
 const app = express();
@@ -22,6 +24,7 @@ const server = async () => {
     }
     app.use(express.json());
     app.use("/api/v1/", routes);
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     app.listen(port, () => {
       console.log(`The server is running on port ${port}`);
     });
