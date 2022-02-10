7b8892b098db59ba7ceaf0c9a2c4cf0cbb7f00bc
tuyizerejean <tuyibobu@gmail.com>
2022-02-10 11:09:11 +0200

    Finalize tests

--- tests/article.test.js
+++ tests/article.test.js
@@ -2,24 +2,64 @@ import chai, { expect } from "chai";
 import chaiHttp from "chai-http";
 import app from "../src/app.js";
 import { generateToken } from "../src/helpers/jwtFunctions.js"
+import { validUser } from "./dummyData.js";
 import "dotenv/config";
 
 chai.use(chaiHttp);
 describe("ARTICLE END-POINT TESTING", () => {
-    let id;
-    it("Should not created the articles", (done) => {
+
+
+  let token = "";
+  it("Experiencing server error", (done) => {
+    chai
+      .request(app)
+      .post("/api/v1/users/login")
+      .send(validUser)
+      .end((err, res) => {
+        token = res.body.accessToken;
+        expect(res).to.have.status([500]);
+        done();
+      });
+  });
+  it("Should loggin the user", (done) => {
+    chai
+      .request(app)
+      .post("/api/v1/users/login")
+      .send(validUser)
+      .end((err, res) => {
+        token = res.body.accessToken;
+        expect(res).to.have.status([200]);
+        done();
+      });
+  });
+
+  let id;
+  it("Should created the articles", (done) => {
+      chai.request(app).post("/api/v1/aritcles")
+          .set("Authorization", `Bearer ${token}`)
+          .set('Content-Type', 'multipart/form-data')
+          .field({ title: 'What about Kigali??', content: 'Kigali is looking very good' })
+          .attach('image', './test.jpg')
+          .end((err, res) => {
+              expect(res).to.have.status([200])
+              id = res.body.data._id;
+              done()
+          })
+  })
+    // let id;
+    it("Should not create the articles without logged in", (done) => {
         chai.request(app).post("/api/v1/aritcles")
             .set("Authorization", `${generateToken({ id: 1 })}`)
             .set('Content-Type', 'multipart/form-data')
-            .field({ title: 'postt1request', content: 'common news' })
+            .field({ title: 'What about Kigali??', content: 'Kigali is looking very good' })
             .attach('image', './test.jpg')
             .end((err, res) => {
                 expect(res).to.have.status([401])
                 // id = res.body.data._id;
                 // console.log(res);
                 done()
-            })
-    })
+            });
+    });
   it("Should retrieve the articles", (done) => {
     chai
       .request(app)
@@ -61,4 +101,16 @@ describe("ARTICLE END-POINT TESTING", () => {
         done();
       });
   });
+
+  it("Should  update one articles", (done) => {
+    chai.request(app).put(`/api/v1/aritcles/6202cfe831321a5e7eaec3cd`)
+        .set("Authorization", `Bearer ${token}`)
+        .set('Content-Type', 'multipart/form-data')
+        .field({ title: 'postt1request', content: 'common news' })
+        .attach('image', './test.jpg')
+        .end((err, res) => {
+            expect(res).to.have.status([200])
+            done()
+        })
+})
 });
